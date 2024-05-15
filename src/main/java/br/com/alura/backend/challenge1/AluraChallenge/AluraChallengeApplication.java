e334586e8f3e9789baa2e12a7dbb9b29034dd404
Guilherme Maciel <guilherme.maciel@exiti.com.br>
2024-05-14 22:44:30 -0300

    Setting properties

---
+++ src/main/java/br/com/alura/backend/challenge1/AluraChallenge/AluraChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.backend.challenge1.AluraChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraChallengeApplication.class, args);
+	}
+
+}
